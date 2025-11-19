9a8f63ff6348710415180024d17a63298851b4be
Sambhav <68431069+SButterscotch@users.noreply.github.com>
2025-11-19 14:04:52 -0800

    lights

--- Chronos/Saved/ShaderDebugInfo/PCD3D_SM6/KB3D_Master_Dupe_13ed1ab798565411/Default/FLocalVertexFactory/FPathTracingMaterialCHS/0_5d7f2b674272b6cc/CompileDXC.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DXC="E:\Games\UE_5.6\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
-IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "E:\Games\UE_5.6\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
-	GOTO :END
-)
-%DXC% -HV 2021 -Zpr -O3 -auto-binding-space 0 -exports PathTracingMaterialCHS -enable-16bit-types -Wno-parentheses-equality -Wno-ignored-attributes -disable-lifetime-markers -T lib_6_6 -Fc PathTracingMaterialHitShader.d3dasm -Fo PathTracingMaterialHitShader.dxil PathTracingMaterialHitShader.usf
-:END
-PAUSE
